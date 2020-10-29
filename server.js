f5e2c01d1a7471542d53cb8bc35751e3d7143cd8
amani <amaniericus@gmail.com>
2020-10-29 21:43:28 +0200

    deploying to heroku

--- server.js
+++ server.js
@@ -6,15 +6,15 @@ import getweatherRoute from './routes/weather'
  require("@babel/core").transform("code", {
   presets: ["@babel/preset-env"],
 });
-
+const port = process.env.PORT || 3000;
 const app = express()
 app.use('/weather',getweatherRoute)
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
-app.listen(3000, function () {
-  console.log('app listening on port 3000!')
+app.listen(port, function () {
+  console.log(`app listening on port ${port}!`)
 })
 
 app.use(bodyParser.urlencoded({ extended: true }));
